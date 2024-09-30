dbb6b1b8ad8f74446d6ddc9fc0cb8d5b336cc9ce
maztas-tech <mata0001@stud.kea.dk>
2024-09-30 13:16:21 +0200

    Initial commit

---
+++ src/main/java/files/kinoxp/KinoXpApplication.java
@@ -0,0 +1,13 @@
+package files.kinoxp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoXpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoXpApplication.class, args);
+    }
+
+}
