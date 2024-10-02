75a88072327d8960f8937a0e305f227cfba7d8ef
saal0001 <saal0001@stud.kea.dk>
2024-10-02 16:47:09 +0200

    added a unit test to always be true

--- src/test/java/files/kinoxp/KinoXpApplicationTests.java
+++ src/test/java/files/kinoxp/KinoXpApplicationTests.java
@@ -1,5 +1,6 @@
 package files.kinoxp;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class KinoXpApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.assertTrue(true);
     }
 
 }
