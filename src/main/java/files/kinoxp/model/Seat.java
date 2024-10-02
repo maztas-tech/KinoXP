a4bd01aad987d46b0067c9c9969edb9ab104d78e
saal0001 <saal0001@stud.kea.dk>
2024-10-02 13:49:53 +0200

    made an enum package and refactored MovieGenre,RoleName and SeatType

--- src/main/java/files/kinoxp/model/Seat.java
+++ src/main/java/files/kinoxp/model/Seat.java
@@ -1,5 +1,6 @@
 package files.kinoxp.model;
 
+import files.kinoxp.model.enums.SeatType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
