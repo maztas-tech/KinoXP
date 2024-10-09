49a1664ea69e20ed4516046abd6a267a1fa1aa2e
saal0001 <saal0001@stud.kea.dk>
2024-10-09 13:40:57 +0200

    made cross origin in all controller classes

--- src/main/java/files/kinoxp/controller/RoleController.java
+++ src/main/java/files/kinoxp/controller/RoleController.java
@@ -3,14 +3,12 @@ package files.kinoxp.controller;
 import files.kinoxp.model.Role;
 import files.kinoxp.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 public class RoleController {
 
     @Autowired
